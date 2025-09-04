424f9e6afad296401b3fd0d2f16df1075198b963
Laura Annijaani <laura.annijaani@tptlive.ee>
2025-09-04 10:12:44 +0300

    Add variable examples

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "Hello World!\n";
+echo "Hello World!\n";
+$name = 'Laura';
+echo 'Hello ' . $name;
+echo "\n";
+$age = 18;
+echo 'Your name is ' . $name . ' and you are ' . $age . ' years old!';
+echo "\n";
+echo "Your name is $name and you are $age years old!\n";
+
+$var = 1;
+var_dump($var);
+$var = 1.3;
+var_dump($var);
+$var = 'Hellõ';
+var_dump($var);
+$var = true;
+var_dump($var);
+$var = false;
+var_dump($var);
+$bar = NULL;
+var_dump($bar);
+
+?>
