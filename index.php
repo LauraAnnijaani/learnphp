e647188415e6c231c41431f5fe85cd140a5a21e2
Laura Annijaani <laura.annijaani@tptlive.ee>
2025-09-12 15:11:14 +0300

    Simple html page with php

--- index.php
+++ index.php
@@ -1,44 +1,22 @@
 <?php
-// library code
-class Job {
-    public function task(Logger $logger){
-        for($i=0; $i<10; $i++){
-            // compicated work something
-            $logger->log("Task nr $i was completed");
 
-        }
-    }
-}
+$name = 'Laura';
 
-class ConsoleLogger implements Logger {
-    public function log($message){
-        echo "$message\n";
-    }
-}
+?>
 
-interface Logger {
-    public function log($message);
-}
-
-// user code
-
-class NothingLogger implements Logger {
-    public function log($message) {
-
-    }
-}
-
-class FileLogger implements Logger {
-    public function log($message) {
-        $file = fopen('log.log', 'a');
-        fwrite($file, "$message\n");
-        fclose($file);
-    }
-}
-
-
-
-
-$job = new Job();
-$logger = new NothingLogger;
-$job->task($logger);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello <?=$name?>!!!</h1>
+    <ul>
+        <?php for($i=0;$i<10;$i++): ?>
+            <li><?$i?></li>
+        <?php endfor ?>
+    </ul>
+</body>
+</html>
