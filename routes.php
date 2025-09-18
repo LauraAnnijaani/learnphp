63acc3a7605238d9981186d45ee9eca6e7f9a0ce
Laura Annijaani <laura.annijaani@tptlive.ee>
2025-09-18 15:32:06 +0300

    Add technology page

--- routes.php
+++ routes.php
@@ -6,3 +6,5 @@ use App\Router;
 Router::addRoute('/', [PublicController::class, 'index']);
 
 Router::addRoute('/us', [PublicController::class, 'us']);
+
+Router::addRoute('/technology', [PublicController::class, 'technology']);
